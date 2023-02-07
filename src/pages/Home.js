b35b9895d019d7c70ba541ed51e7d5a4e8f6b096
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-08 02:07:53 +0530

    changed text positioning on image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const Home = () => {
   return (<>
@@ -6,8 +7,14 @@ const Home = () => {
       <div className="container-xxl">
         <div className="row">
           <div className="col-6">
-            <div className="main-banner-content p-3">
-              <img src="images/main-banner.jpg" alt="main banner" />
+            <div className="main-banner p-3 position-relative">
+              <img src="images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner" />
+              <div className="main-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From ₹59999.00 or ₹5999/mo. </p>
+                <Link to="/">BUY NOW</Link>
+              </div>
             </div>
           </div>
         </div>
