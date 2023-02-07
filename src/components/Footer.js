c48916b0bdc9cdfcb82c24d8b470fab974961662
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-08 01:08:29 +0530

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Footer = () => {
   return (<>
     <footer className="py-4">
@@ -29,9 +29,65 @@ const Footer = () => {
         </div>
     </footer>
     
-    <footer className="py-3"></footer>
+    <footer className="py-4">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-4">
+            <h4 className="text-white mb-4">Contact Us</h4>
+            <div>
+              <address className="text-white fs-6">
+                161 Downing Street, <br /> Mumbai, Maharashtra <br />
+                Pin - 4000001
+              </address>
+              <a href="tel:+91 9876543210" className="mt-4 d-block mb-2 text-white">
+                +91 9876543210
+              </a>
+              <a href="mailto:shubham-tiwary@outlook.com" className="mt-4 d-block mb-2 text-white">
+                shubham-tiwary@outlook.com
+              </a>
+              <div className="social-icons">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <h4 className="text-white mb-4">Information</h4>
+            <div className="footer-link d-flex flex-column">
+              <Link to="/" className="text-white py-2 mb-1">Privacy Policy</Link>
+              <Link to="/" className="text-white py-2 mb-1">Refund Policy</Link>
+              <Link to="/" className="text-white py-2 mb-1">Shipping Policy</Link>
+              <Link to="/" className="text-white py-2 mb-1">Terms & Conditions</Link>
+              <Link to="/" className="text-white py-2 mb-1">Blogs</Link>
+            </div>
+          </div>
+          <div className="col-3">
+            <h4 className="text-white mb-4">Account</h4>
+            <div className="footer-link d-flex flex-column">
+              <Link to="/" className="text-white py-2 mb-1">Search</Link>
+              <Link to="/" className="text-white py-2 mb-1">About Us</Link>
+              <Link to="/" className="text-white py-2 mb-1">FAQ</Link>
+              <Link to="/" className="text-white py-2 mb-1">Contact</Link>
+              <Link to="/" className="text-white py-2 mb-1">Size Chart</Link>
+            </div>
+          </div>
+          <div className="col-2">
+            <h4 className="text-white mb-4">Quick Links</h4>
+            <div className="footer-link d-flex flex-column">
+              <Link to="/" className="text-white py-2 mb-1">Accessories</Link>
+              <Link to="/" className="text-white py-2 mb-1">Laptops</Link>
+              <Link to="/" className="text-white py-2 mb-1">Headphones</Link>
+              <Link to="/" className="text-white py-2 mb-1">Smart Watches</Link>
+              <Link to="/" className="text-white py-2 mb-1">Tablets</Link>
+            </div>
+          </div>
+        </div>
+     </div>
+    </footer>
     
-    <footer className="py-3">
+    <footer className="py-4">
       <div className="container-xxl">
         <div className="row">
           <div className="col-12">
