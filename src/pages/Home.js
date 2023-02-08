8b6edaa13c575efaff9111c38edc5ca6edd93d4d
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-09 01:50:27 +0530

    added side cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,6 +17,42 @@ const Home = () => {
               </div>
             </div>
           </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+            <div className="small-banner p-3 position-relative">
+              <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner" />
+              <div className="small-banner-content position-absolute">
+                <h4>NEW ARRIVAL</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From ₹59999.00 or ₹5999/mo. </p>
+              </div>
+            </div>
+            <div className="small-banner p-3 position-relative">
+              <img src="images/catbanner-02.jpg" className="img-fluid rounded-3" alt="main banner" />
+              <div className="small-banner-content position-absolute">
+                <h4>NEW ARRRIVAL</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From ₹59999.00 or ₹5999/mo. </p>
+              </div>
+            </div>
+            <div className="small-banner p-3 position-relative">
+              <img src="images/catbanner-03.jpg" className="img-fluid rounded-3" alt="main banner" />
+              <div className="small-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From ₹59999.00 or ₹5999/mo. </p>
+              </div>
+            </div>
+            <div className="small-banner p-3 position-relative">
+              <img src="images/catbanner-04.jpg" className="img-fluid rounded-3" alt="main banner" />
+              <div className="small-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS.</h4>
+                <h5>iPad S13+ Pro.</h5>
+                <p>From ₹59999.00 or ₹5999/mo. </p>
+              </div>
+            </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
