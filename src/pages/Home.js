b2c1b0e6bfae20ffd616762523eaf4798cb6ebc1
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-13 23:15:52 +0530

    added images template

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -64,35 +64,35 @@ const Home = () => {
             <div className="col-12">
               <div className="services d-flex align-items-center justify-content-between">
                 <div>
-                  <img src="" alt="services" />
+                  <img src="images/service.png" alt="services" />
                     <div>
                       <h6></h6>
                       <p></p>
                     </div>
                 </div>
                 <div>
-                  <img src="" alt="services" />
+                  <img src="images/service-02.png" alt="services" />
                     <div>
                       <h6></h6>
                       <p></p>
                     </div>
                 </div>
                 <div>
-                  <img src="" alt="services" />
+                  <img src="images/service-03.png" alt="services" />
                     <div>
                       <h6></h6>
                       <p></p>
                     </div>
                 </div>
                 <div>
-                  <img src="" alt="services" />
+                  <img src="images/service-04.png" alt="services" />
                     <div>
                       <h6></h6>
                       <p></p>
                     </div>
                 </div>
                 <div>
-                  <img src="" alt="services" />
+                  <img src="images/service-05.png" alt="services" />
                     <div>
                       <h6></h6>
                       <p></p>
