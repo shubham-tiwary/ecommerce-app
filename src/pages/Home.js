d175a85b5850bcf137d45652431e210de054c9a4
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-18 22:33:12 +0530

    services portion changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -63,35 +63,35 @@ const Home = () => {
           <div className="row">
             <div className="col-12">
               <div className="services d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service.png" alt="services" />
                     <div>
                       <h6>Free Delivery</h6>
                       <p className="mb-0">On orders above ₹599</p>
                     </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-02.png" alt="services" />
                     <div>
                       <h6>Daily Surprise Offers</h6>
                       <p className="mb-0">Upto 60% off</p>
                     </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-03.png" alt="services" />
                     <div>
                       <h6>24x7 Support</h6>
                       <p className="mb-0">Shop with an expert</p>
                     </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-04.png" alt="services" />
                     <div>
                       <h6>Affordable Prices</h6>
                       <p className="mb-0">Get Factory Default Prices</p>
                     </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
+                <div className="d-flex align-items-center gap-15">
                   <img src="images/service-05.png" alt="services" />
                     <div>
                       <h6>Secure Payments</h6>
