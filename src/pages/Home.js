9b8b5afd6b0cdf93ce53fd1cfea34f62280b4265
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-20 02:42:33 +0530

    finnlised brand section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -179,8 +179,31 @@ const Home = () => {
         <div className="row">
           <div className="col-12">
             <div className="marquee-inner-wrapper card-wrapper">
-              <Marquee>
-                I can be a React component.
+              <Marquee className="d-flex">
+                <div className="mx-4 w-25">
+                  <img src="images/brand-01.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-02.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-03.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-04.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-05.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-06.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-07.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25">
+                  <img src="images/brand-08.png" alt="brand" />
+                </div>
               </Marquee>
             </div>
           </div>
