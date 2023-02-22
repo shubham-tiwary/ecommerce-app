7b264301309ffbac70239b29a78ac711f966c187
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-23 03:09:52 +0530

    updated news/blog section

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const BlogCard = () => {
   return (
@@ -9,6 +10,9 @@ const BlogCard = () => {
             </div>
             <div className="blog-content">
               <p className="date"></p>
+              <h5 className="title"></h5>
+              <p className="description"></p>
+              <Link>Read More</Link>
             </div>
         </div>
     </div>
