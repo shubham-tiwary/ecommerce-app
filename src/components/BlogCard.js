0577834b276c6c63d5e4d2229669265861188d47
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-02-27 23:53:01 +0530

    updated news/blog section

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -11,7 +11,7 @@ const BlogCard = () => {
             <div className="blog-content">
               <p className="date">25 February, 2023</p>
               <h5 className="title">A beautiful Sunday Morning Renaissance</h5>
-              <p className="desc">LI</p>
+              <p className="desc">Lorem Ipsum Text</p>
               <Link to="/" className="button">Read More</Link>
             </div>
         </div>
