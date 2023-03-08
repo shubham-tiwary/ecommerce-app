a20e0fb08502e729663cfd5df3ea5d945c19a8ed
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-03-08 23:19:42 +0530

    product card changes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ const ProductCard = () => {
     <div className="col-3">
         <div className="product-card">
           <div className="product-image">
-            <img src="images/watch.jpg" alt="product image"></img>
+            <img src="images/watch.jpg" alt="product image" />
           </div>
         </div>
     </div>
