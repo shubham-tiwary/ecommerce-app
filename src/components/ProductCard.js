f1f5f0c5faa0fe522e9246dd47f15f88e48b702a
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-03-08 23:18:20 +0530

    product card changes

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,11 @@ import React from 'react'
 const ProductCard = () => {
   return (
     <div className="col-3">
-        <div className="product-card"></div>
+        <div className="product-card">
+          <div className="product-image">
+            
+          </div>
+        </div>
     </div>
   )
 }
