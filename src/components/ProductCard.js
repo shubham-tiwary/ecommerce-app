00559ffc76d61efe00fec5f2e4716ab38897a52a
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-03-22 20:25:36 +0530

    added react-rating-stars-component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -13,6 +13,7 @@ const ProductCard = () => {
             <h5 className="product-title">
               Multi-colored kids headphones for students
             </h5>
+            <ReactStars count={5} size={24} activeColor="#ffd700" />
             <p className="price">₹499</p>
           </div>
         </div>
