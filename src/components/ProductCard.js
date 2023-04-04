074381ebfe44e552cd6cb2d518c608510bb46668
ShubhamTiwary <shubham.tiw007@gmail.com>
2023-04-04 23:56:29 +0530

    added react-rating-stars-component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ import ReactStars from "react-rating-stars-component";
 const ProductCard = () => {
   return (
     <div className="col-3">
-        <div className="product-card">
+        <div className="product-card position-relative">
           <div className="product-image">
             <img src="images/watch.jpg" alt="product image" />
           </div> 
